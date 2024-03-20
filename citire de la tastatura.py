e04bfc8912c95bb2a1c6567e734d96b12524c719
mvisovan <155732373+mvisovan@users.noreply.github.com>
2024-03-20 09:44:27 +0200

    Add files via upload

---
+++ citire de la tastatura.py
@@ -0,0 +1,8 @@
+lungime = input('lungimea latura')
+latime = input('latimea latura')
+
+
+
+print(f'dreptunghiul are o latura de {lungime}' )
+print(f'dreptunghiul are o latura de {latime} ')
+print(f'aria dreptunghiului este {lungime}*{latime}')
